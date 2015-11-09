a98c3135bf7b6cd37bf7ae1f9c28f8f71a78788a
Klas Eskilson <klas.eskilson@gmail.com>
2015-11-09 13:50:52 +0100

    Remove unused helpers

--- helpers/rgb2ycc.m
+++
@@ -1,6 +0,0 @@
-function [YCbCr] = rgb2ycc(rgb)
-
-YCbCr = [ 16; 128; 128;] + [ 0.183  0.614  0.062; 
-                             -0.101 -0.339  0.439; 
-                              0.439 -0.339 -0.040; ] * rgb;
-YCbCr = uint8(YCbCr);
