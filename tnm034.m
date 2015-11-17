ef888bfe7c6effeac9e7ff236a697f9ce88308b7
tistatos <tistatos@gmail.com>
2015-11-17 14:35:42 +0100

    Avoided crash if no mouth or eyes are detected
    also added a threshold parameter to the skin model

--- tnm034.m
+++ tnm034.m
@@ -11,24 +11,28 @@ function id = tnm034(img)
     ycc = rgb2ycbcr(img);
     yccorig = ycc;
     ycc = chromaTransformation(ycc);
-    mask = skinModel(ycc);
     
+    le = [0 0];
+    re = le;
+    m = le;
+    skinModelThreshold = 1.5;
+    mask = skinModel(ycc, skinModelThreshold);
+
     eye = eyeMap(yccorig);
     mouth = mouthMap(yccorig, mask);    
-    
+
     eye = normalize(double(eye).*double(mask(:,:,1)), 255);
     eye = uint8(eye);
-
     [le,re, m] = faceTriangle(faceCrop(eye,mask), faceCrop(mouth,mask));
     cropped = faceCrop(img,mask);
-    
+
     polygon = int32([m(1), m(2), le(1), le(2), re(1), re(2)]);
-    J = insertShape(cropped, 'Polygon', polygon, 'Color', 'red', 'Opacity', 0.7);
+    J = insertShape(cropped, 'FilledPolygon', polygon, 'Color', 'red', 'Opacity', 0.7);
     
     subplot(2,2,1) , subimage(J);
     subplot(2,2,2) , subimage(img .* uint8(mask));
-    subplot(2,2,3) , subimage(eye > 230);
-    subplot(2,2,4) , subimage(mouth > 150);
+    subplot(2,2,3) , subimage(faceCrop(eye,mask));
+    subplot(2,2,4) , subimage(mouth);
     
     id = 666;
 end
