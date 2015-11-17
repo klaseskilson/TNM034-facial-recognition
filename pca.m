fa9c34a1403b234a7060a23030c5d4275f73970c
Klas Eskilson <klas.eskilson@gmail.com>
2015-11-17 11:40:10 +0100

    Adjust indenting in PCA

--- pca.m
+++ pca.m
@@ -2,23 +2,23 @@
 function [eigenVector, meanImg] = pca(Img)
 %   img - the image to analyse
 
-        [w, h] = size(Img);
-        meanImg = mean(Img(:));
-        %Repmat function returns a repeated copy of the array.
-        Xm = double(Img) - repmat(meanImg, w, h);
+    [w, h] = size(Img);
+    meanImg = mean(Img(:));
+    %Repmat function returns a repeated copy of the array.
+    Xm = double(Img) - repmat(meanImg, w, h);
 
-        if(w > h)
-            S = Xm'*Xm;
-            %Retrieve Eigen Values
-            [eigenVector, D] = eig(S);
-        else
-            S = Xm*Xm';
-            [eigenVector, D] = eig(S);
-            eigenVector = Xm'*eigenVector;
-            D = normalize(D, 255);
-        end
-        
-        %Sort Eigenvalues
-        [D, i] = sort(diag(D), 'descend');
-        eigenVector = eigenVector(:,i);
+    if(w > h)
+        S = Xm'*Xm;
+        %Retrieve Eigen Values
+        [eigenVector, D] = eig(S);
+    else
+        S = Xm*Xm';
+        [eigenVector, D] = eig(S);
+        eigenVector = Xm'*eigenVector;
+        D = normalize(D, 255);
+    end
+
+    %Sort Eigenvalues
+    [D, i] = sort(diag(D), 'descend');
+    eigenVector = eigenVector(:,i);
 end
