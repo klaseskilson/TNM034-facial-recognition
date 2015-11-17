c45eb378771542239e827d4249c38ae2baed0073
Carl Englund <englund92@gmail.com>
2015-11-17 14:40:35 +0100

    Optimized mean calculations in mouthMap

--- mouthMap.m
+++ mouthMap.m
@@ -3,17 +3,16 @@ function [mouthMap] = mouthMap(img, faceMask)
    
     Cb = im2double(img(:,:,2));
     Cr = im2double(img(:,:,3));
-    mask = double(faceMask(:,:,1));
     CrSquared = Cr.^2;
     CrCb = (Cr./Cb);
 
     CrSquared = normalize(CrSquared, 255);
     CrCb = normalize(CrCb, 255);
     
-    eta = 0.95 * mean(mean(CrSquared))/mean(mean(CrCb));
+    eta = 0.95 * mean(CrSquared(:))/mean(CrCb(:));
     
     mouthMap = CrSquared.*((CrSquared -(eta.*CrCb)).^2);
-    mouthMap = uint8(255*mouthMap/max(max(mouthMap)));
+    mouthMap = uint8(255*mouthMap/max(mouthMap(:)));
 
     diskSize = 10;
     kernel = strel('disk', diskSize);
