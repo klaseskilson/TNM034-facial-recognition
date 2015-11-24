e21dc1e890d6a1befa5c0d5f5e00e7b7717e5f10
Klas Eskilson <klas.eskilson@gmail.com>
2015-11-24 11:00:57 +0100

    Add optional height and widht param to alignFace

--- alignFace.m
+++ alignFace.m
@@ -1,21 +1,19 @@
-function [ transformedImage ] = alignFace( image, pts )
+function [ transformedImage ] = alignFace( image, pts, height, width )
 %alignFace alignfaces to match template image
 
-height = 256;
-width = height*0.66;
+    if nargin < 3
+        height = 256;
+        width = height*0.66;
+    end
 
+    matching = [ width*0.25,  height*0.3, 1;
+                 width*0.75, height*0.3, 1;
+                 width*0.5, height*0.7, 1;];
 
-matching = [ width*0.25,  height*0.3, 1;
-             width*0.75, height*0.3, 1;
-             width*0.5, height*0.7, 1;];
-
-Rcb = imref2d([height,uint32(width)]);
-transform = mldivide(pts,matching);
-transform(:,3) = [0; 0; 1];
-transform = affine2d(transform);
-transformedImage = imwarp(image,transform, 'OutputView', Rcb, 'SmoothEdges', true);
-
-
-
+    Rcb = imref2d([height,uint32(width)]);
+    transform = mldivide(pts,matching);
+    transform(:,3) = [0; 0; 1];
+    transform = affine2d(transform);
+    transformedImage = imwarp(image,transform, 'OutputView', Rcb, 'SmoothEdges', true);
 end
 
