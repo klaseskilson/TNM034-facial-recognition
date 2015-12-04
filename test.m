629048461e64869e20fe8e29a4488ae00854df43
tistatos <tistatos@gmail.com>
2015-12-04 10:04:08 +0100

    Added threshold value to recognition
    Also stats for false negative and false positive added

--- test.m
+++ test.m
@@ -3,7 +3,10 @@ files = dir(fullfile(dirname, '*.jpg'));
 files = {files.name}';
 correct = 0;
 nofound = 0;
+falseNegative = 0;
+falsePositive = 0;
 angle = 5;
+threshold = 200;
 for i=1:numel(files)
     clear img fname res;
     fname = fullfile(dirname, files{i});
@@ -11,28 +14,35 @@ for i=1:numel(files)
     number = str2num(fname(end-5:end-4));
     for k=-angle:angle
         % destroy the image!
-        % modifiedImage = imrotate(img, k);
-        modifiedImage = img * (1 - k * 0.01);
+        modifiedImage = imrotate(img, k);
+        %modifiedImage = img * (1 - k * 0.01);
 
         [res, info] = tnm034(modifiedImage);
-
-        if(res == number)
+        w = info(1,2);
+        id = info(1,1);
+        if(w > threshold)
+            'Over treshold!'
+            if(res == number)
+                falseNegative = falseNegative + 1;
+            end
+        elseif(res == number)
             disp(['Match for file "' fname '": ' num2str(res)]);
             correct = correct +1;
         else
+            falsePositive = falsePositive + 1;
             disp(['No Match for "' fname '"! Got ' num2str(res) ', expected ' num2str(number) ]);
         end
-        if(res ~=-1)
-            id = info(:,1);
-            w = info(:,2);
-            id(1:3)'
-            w(1:3)'
-        else
-            nofound = nofound + 1;
-        end
+%         if(res ~=-1)
+%             id = info(:,1);
+%             w = info(:,2);
+%             id(1:3)'
+%             w(1:3)'
+%         else
+%             nofound = nofound + 1;
+%         end
     end
 end
 correct
 correctness = correct/(numel(files)*(1+angle*2))
-nofound
-
+falseNegative
+falsePositive
